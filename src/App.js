6f0c6e335f42488f49197ff406d16b0efc0d4074
Your Name <jgbharris@hotmail.co.uk>
2021-01-27 19:25:50 +0000

    autocomplete working to show stations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { createStore } from 'redux'
+import manuel from 'manuel'
 import './App.css';
+import { Provider } from 'react-redux'
+import Autocomplete from "./Autocomplete";
 
 function App() {
   const [data, setData] = useState([]);
@@ -10,11 +14,39 @@ function App() {
         .then((data) => setData(data));
 }, []);
 
+
+
 console.log("data", data)
+console.log("data.data", data.data)
+const stations = data.data
+
+
+// const stationsArray = []
+// // eslint-disable-next-line
+// stations ? stations.forEach((obj) => {
+// 	Object.keys(obj).forEach((key) => {
+//     stationsArray.push(obj[key]);
+//   });
+// }) : null;
+
+const stationsArray = []
+if (stations) {
+  for (let i = 0; i < stations.length; i++) {
+    stationsArray.push(stations[i]["name"])
+    };
+}
+
+
+// console.log("stationsArray", stationsArray)
+console.log("stationsArray", stationsArray)
 
   return (
     <div className="App">
      <h1>TEST</h1>
+     <Autocomplete
+        options={stationsArray
+        }
+      />
     </div>
   );
 }
